4d41fa5e0625a7f8296f60be8410bef7296958dd
Anthony Nandaa <anthony.nandaa@andela.com>
2016-01-26 15:04:56 +0300

    add new reverse function

--- str_reverse.py
+++ str_reverse.py
@@ -1,9 +1,20 @@
-def reverse(s):
+def reverse(w):
     '''Returns the reverse of a string
     using slicing
     '''
-    return s[::-1]
+    return w[::-1]
+
+def swap(list_, i, j):
+    list_[i], list_[j] = list_[j], list_[i]
+
+def reversex(s):
+    new_s = list(s)
+    length = len(new_s)
+    for i in range(length // 2):
+        swap(new_s, i, length - i - 1)
+
+    return "".join(new_s)
 
 # examples
-print reverse("andela")
+print reversex("andela")
 print reverse("hello")
